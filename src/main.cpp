0f8549c7029dd2b545ba68c397788b5c386963d1
Joel W. Blomster <joel.blomster@gilion.com>
2024-12-25 17:10:25 +0100

    printing all labels... now I need to organize them

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,23 @@
 #include <fstream>
-#include <vector>
-
-class NeuralNetwork {
-    // remember to transpose matrice before multiplication -- great for
-    // performance
-  public:
-    NeuralNetwork(int input_size, int hidden_size, int output_size);
-    std::vector<float> forward(const std::vector<float>& input);
-    void backward(const std::vector<float>& input,
-                  const std::vector<float>& target);
-    void update_weights(float learning_rate);
-
-  private:
-    std::vector<std::vector<float>> weights_input_hidden;
-    std::vector<std::vector<float>> weights_hidden_output;
-    std::vector<float> hidden_layer;
-    std::vector<float> output_layer;
-};
+#include <iostream>
 
 int main() {
-    std::ifstream labels("../mnist/train-labels.idx1-ubyte");
-    std::ifstream images("../mnist/train-labels.idx1-ubyte");
+    std::ifstream labels("mnist/train-labels.idx1-ubyte");
+    std::ifstream images("mnist/train-labels.idx1-ubyte");
+    std::string line;
+
+    if (labels) {
+        labels.seekg(0, labels.end);
+        int length = labels.tellg();
+        int rows = length - 8;
+        std::cout << "Total count: " << rows << std::endl;
+        for (int i = 8; i < rows; i++) {
+            char c;
+            labels.seekg(i, labels.beg);
+            labels.read(&c, 1);
+            std::cout << "Label: " << (int)c << std::endl;
+        }
+    }
+
+    return 0;
 }
