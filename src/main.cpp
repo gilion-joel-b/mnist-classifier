0fcf8256f134e80ab7ec8b6c03f31b4a99ccb953
Joel W. Blomster <joel.blomster@gilion.com>
2024-12-25 19:05:39 +0100

    updated function

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,44 @@ int main() {
                          std::ios_base::binary);
     std::ifstream images("mnist/train-labels.idx1-ubyte",
                          std::ios_base::binary);
-    std::string line;
+
+    int labelsLength = labels.tellg();
+    int labelValues[labelsLength];
 
     if (labels) {
         labels.seekg(0, labels.end);
-        int length = labels.tellg();
-        int rows = length - 8;
+
+        int rows = labelsLength - 8;
         std::cout << "Total count: " << rows << std::endl;
         for (int i = 8; i < rows; i++) {
             char c;
             labels.seekg(i, labels.beg);
             labels.read(&c, 1);
-            std::cout << "Label: " << (int)c << std::endl;
+            labelValues[i] = c;
         }
         std::cout << "Row count: " << rows << std::endl;
     }
 
     return 0;
+}
+
+int* loadLabels(std::string path) {
+    std::ifstream labels(path, std::ios_base::binary);
+
+    int labelsLength = labels.tellg();
+    int labelValues[labelsLength];
+
+    if (labels) {
+        labels.seekg(0, labels.end);
+
+        int rows = labelsLength - 8;
+        for (int i = 8; i < rows; i++) {
+            char c;
+            labels.seekg(i, labels.beg);
+            labels.read(&c, 1);
+            labelValues[i] = c;
+        }
+    }
+
+    return labelValues;
 }
