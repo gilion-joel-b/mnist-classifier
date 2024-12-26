1f57aa3dc65cfa91e871b49fae73265ebe88ec5b
Joel W. Blomster <joel.blomster@gilion.com>
2024-12-26 15:49:20 +0100

    iteratore images

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,26 @@ auto loadImages(std::string path) {
     std::ifstream images(path, std::ios_base::binary);
     std::vector<std::vector<int>> imageValues;
 
+    if (images) {
+        images.seekg(0, images.end);
+        int imagesLength = images.tellg();
+        images.seekg(16, images.beg);
+
+        int rows = imagesLength - 16;
+
+        for (int i = 16; i < rows; i++) {
+            std::vector<int> image;
+
+            for (int j = 0; j < 28; j++) {
+                char c;
+                images.seekg(i, images.beg);
+                images.read(&c, 1);
+                image.push_back(c);
+            }
+            imageValues.push_back(image);
+        }
+
+    }
 }
 
 int main() {
