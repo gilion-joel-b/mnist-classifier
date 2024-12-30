703ea61a2deec0c1d2e12a264d0495e5189bfeea
Joel W. Blomster <joel.blomster@gilion.com>
2024-12-30 21:17:52 +0100

    test in memory

--- src/main.cpp
+++ src/main.cpp
@@ -97,8 +97,25 @@ std::vector<char> loadLabelsTwo(const std::string& path) {
 // }
 //
 int main() {
+    using std::chrono::duration;
+    using std::chrono::duration_cast;
+    using std::chrono::high_resolution_clock;
+    using std::chrono::milliseconds;
+
     std::ifstream images("mnist/train-labels.idx1-ubyte",
                          std::ios_base::binary);
+    auto t1 = high_resolution_clock::now();
+
     auto labels = loadLabels("mnist/train-labels.idx1-ubyte");
-    auto labelsTwo = loadLabelsTwo("mnist/train-labels.idx1-ubyte");
+    auto t2 = high_resolution_clock::now();
+    /* Getting number of milliseconds as an integer. */
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+
+    /* Getting number of milliseconds as a double. */
+    duration<double, std::milli> ms_double = t2 - t1;
+
+    std::cout << ms_int.count() << "ms\n";
+    std::cout << ms_double.count() << "ms\n";
+
+    return 0;
 }
