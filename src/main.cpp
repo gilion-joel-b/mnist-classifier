b00e3f3f99d1b6e3f800a9744cccbfac97be713f
Joel W. Blomster <joel.blomster@gilion.com>
2025-01-04 12:33:28 +0100

    load images as well

--- src/main.cpp
+++ src/main.cpp
@@ -8,30 +8,7 @@
 #include <string>
 #include <vector>
 
-std::vector<int> loadLabelsOne(std::string path) {
-    std::ifstream labels(path, std::ios_base::binary);
-    if (!labels) {
-        throw std::runtime_error("Could not open file");
-    }
-
-    labels.seekg(0, labels.end);
-    int labelsLength = labels.tellg();
-    labels.seekg(0, labels.beg);
-
-    int rows = labelsLength - 8;
-    char buffer[labelsLength];
-    std::vector<int> labelValues(rows);
-
-    labels.read(buffer, labelsLength);
-
-    for (int i = 8; i < labelsLength; i++) {
-        labelValues[i - 8] = buffer[i];
-    }
-
-    return labelValues;
-}
-
-std::vector<int> loadLabelsThree(const std::string& path) {
+std::vector<int> loadLabels(const std::string& path) {
     std::ifstream labels(path, std::ios_base::binary);
     if (!labels) {
         throw std::runtime_error("Could not open file");
@@ -80,67 +57,58 @@ void benchmark(std::string ref, std::string arg,
     std::cout << ms_double.count() << "ms\n";
 }
 
+auto loadImages(std::string path) {
+    std::ifstream images(path, std::ios_base::binary);
+
+    if (!images) {
+        throw std::runtime_error("Could not open file");
+    }
+
+    // Need to offset by 16 to skip the header and rows x cols //
+    // After offsetting we know that each tensor is 28x28 matrix of pixels
+    //
+    // We actually want to return a list of 28x28 matrices
+    // First we need to iterate the the rows, and collect 28 at a time into
+    // a matrix.
+    //
+    // All values are unsigned bytes, in a list of 784 values ordered by
+    // rows.
+    //
+    // We can use a buffer to read all the values at once and then iterate
+    // through the buffer to create the meatrices.
+    //
+    images.seekg(0, images.end);
+    int imagesLength = images.tellg();
+    images.seekg(16, images.beg);
+
+    int imageSize = 28 * 28;
+    int numImages = (imagesLength - 16);
+
+    std::vector<unsigned char> buffer(imageSize * numImages);
+    images.read(reinterpret_cast<char*>(buffer.data()), numImages);
+
+    // Then instead of returning a list of 28x28 matrices, we can return a list
+    // of all values.
+    // This is because this way we can store it in contiguous memory.
+    // This is important for performance.
+    std::vector<int> imageValues(imageSize);
+    for (int i = 0; i < numImages; i++) {
+        imageValues[i] = static_cast<int>(buffer[i]);
+    }
+
+    return imageValues;
+}
+
 int main() {
-    auto path = "mnist/train-labels.idx1-ubyte";
-    benchmark("one", path, &loadLabelsOne);
-    benchmark("three", path, &loadLabelsThree);
+    auto labelsPath = "mnist/train-labels.idx1-ubyte";
+    auto imagesPath = "mnist/train-images.idx3-ubyte";
+    auto labels = loadLabels(labelsPath);
+    auto images = loadImages(imagesPath);
 
-    auto l1 = loadLabelsOne(path);
-    auto l2 = loadLabelsThree(path);
-
-    // std::cout << "l1: " << l1.size() << " l2:" << l2.size()
-    //           << " l3:" << l3.size() << std::endl;
-
-    // for (int i = 0; i < l1.size(); i++) {
-    //     std::cout << l1[i] << " " << l2[i] << " " << l3[i] << std::endl;
-    // }
-
-    assert(l1.size() != 0);
-    assert(l2.size() != 0);
-
-    assert(l1 == l2);
+    for (const auto& l : labels) {
+        std::cout << l << std::endl;
+    }
 
     return 0;
 }
 
-// auto loadImages(std::string path) {
-//     std::ifstream images(path, std::ios_base::binary);
-//
-//     if (!images) {
-//         throw std::runtime_error("Could not open file");
-//     }
-//
-//     // Need to offset by 16 to skip the header and rows x cols //
-//     // After offsetting we know that each tensor is 28x28 matrix of pixels
-//     //
-//     // We actually want to return a list of 28x28 matrices
-//     // First we need to iterate the the rows, and collect 28 at a time into
-//     // a matrix.
-//     //
-//     // All values are unsigned bytes, in a list of 784 values ordered by
-//     // rows.
-//     //
-//     // We can use a buffer to read all the values at once and then iterate
-//     // through the buffer to create the meatrices.
-//     //
-//     // However this approach is not memory effieicent, to avoid this we can
-//     // initialize empty vectors, and then stream the values per image which
-//     is
-//     // 28x28 = 784 values.
-//     //
-//     // This is much more memory efficient, and better for large datasets
-//
-//     images.seekg(0, images.end);
-//     int imagesLength = images.tellg();
-//     images.seekg(16, images.beg);
-//
-//     int imageSize = 28 * 28;
-//     int numImages = (imagesLength - 16) / imageSize;
-//
-//     std::vector<std::vector<std::vector<int>>> imagesValues(
-//         numImages, std::vector<std::vector<int>>(28, std::vector<int>(28)));
-//
-//     std::vector<unsigned char> buffer(imageSize * numImages);
-//     std::vector<unsigned char> imageBuffer(imageSize);
-// }
-//
